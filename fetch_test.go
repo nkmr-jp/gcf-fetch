710434779a632e96fc8e77deebb6be377ac97a32
nkmr-jp <nkmr.git@gmail.com>
2022-07-22 11:32:27 +0900

    :art: Update test

--- fetch_test.go
+++ fetch_test.go
@@ -14,53 +14,70 @@ import (
 )
 
 func TestRun(t *testing.T) {
-	test := NewTestFetch()
-	test.setup(t)
-	objPath := "api.github.com/users/github"
-	ctx := context.Background()
-	client, _ := storage.NewClient(ctx)
+	test := NewTestFetch(t)
 
-	// Get generation before send pubsub message.
-	var preGeneration int64
-	rc, err := client.Bucket(os.Getenv("BUCKET_NAME")).Object(objPath).NewReader(ctx)
-	defer rc.Close() // nolint
-	if err == nil {
-		preGeneration = rc.Attrs.Generation
-	}
+	t.Run("single url", func(t *testing.T) {
+		objPath := "api.github.com/users/github"
+		pubsubData := "https://" + objPath
+		ctx := context.Background()
 
-	// Send pubsub message
-	if err := fetch.Run(ctx, test.event); err != nil {
-		assert.Fail(t, err.Error())
-	}
+		// Send pubsub message1
+		if err := fetch.Run(ctx, test.event(pubsubData)); err != nil {
+			assert.Fail(t, err.Error())
+		}
+		reader1 := test.getObject(ctx, objPath)
 
-	// Get generation after send pubsub message.
-	rc2, err := client.Bucket(os.Getenv("BUCKET_NAME")).Object(objPath).NewReader(ctx)
-	defer rc.Close() // nolint
-	if err != nil {
-		assert.Fail(t, err.Error())
-	}
+		// Send pubsub message2
+		if err := fetch.Run(ctx, test.event(pubsubData)); err != nil {
+			assert.Fail(t, err.Error())
+		}
+		reader2 := test.getObject(ctx, objPath)
 
-	assert.NotEqual(t, preGeneration, rc2.Attrs.Generation)
+		// Get generation after send pubsub message.
+		assert.NotEqual(t, reader1.Attrs.Generation, reader2.Attrs.Generation)
+	})
+
+	t.Run("multi url", func(t *testing.T) {
+		t.Skip()
+		// if got := add(tt.args.a, tt.args.b); got != tt.want {
+		// 	t.Errorf("add() = %v, want %v", got, tt.want)
+		// }
+	})
 }
 
 type TestFetch struct {
-	event event.Event
+	t *testing.T
 }
 
-func NewTestFetch() *TestFetch {
-	return &TestFetch{}
+func NewTestFetch(t *testing.T) *TestFetch {
+	return &TestFetch{t}
 }
 
-func (f *TestFetch) setup(t *testing.T) {
+func (f *TestFetch) getObject(ctx context.Context, objPath string) *storage.Reader {
+	client, _ := storage.NewClient(ctx)
+	reader, err := client.Bucket(os.Getenv("BUCKET_NAME")).Object(objPath).NewReader(ctx)
+	defer func(rc *storage.Reader) {
+		err := rc.Close()
+		if err != nil {
+			assert.Fail(f.t, err.Error())
+		}
+	}(reader)
+	if err != nil {
+		assert.Fail(f.t, err.Error())
+	}
+	return reader
+}
+
+func (f *TestFetch) event(data string) event.Event {
 	msg := pubsub.MessagePublishedData{
 		Message: &pubsub.Message{
-			Data: []byte("https://api.github.com/users/github"),
+			Data: []byte(data),
 		},
 	}
-
-	f.event = event.New()
-	f.event.SetDataContentType("application/json")
-	if err := f.event.SetData(f.event.DataContentType(), msg); err != nil {
-		assert.Fail(t, err.Error())
+	e := event.New()
+	e.SetDataContentType("application/json")
+	if err := e.SetData(e.DataContentType(), msg); err != nil {
+		assert.Fail(f.t, err.Error())
 	}
+	return e
 }
